03099a96dd9b093986e100e9fd73cbcf79129ea0
nilanjanb3 <nilanjan.bhattacharjee77@gmail.com>
2023-02-24 12:44:44 +0530

    modified backend

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,22 +1,8 @@
-resource "aws_s3_bucket" "terraform-state" {
+# resource "aws_s3_bucket" "terraform-state" {
 
-    bucket = "poc-adot-remote-state"
+#     bucket = "poc-adot-remote-state"
 
-    lifecycle {
-      prevent_destroy = true
-    }
-
-
-    # versioning {
-    #   enabled = true
-    # }
-    # server_side_encryption_configuration {
-    #   rule {
-    #     apply_server_side_encryption_by_default {
-    #       sse_algorithm = "AES256"
-    #     }
-    #   }
-    # }
-
-  
-}
+#     lifecycle {
+#       prevent_destroy = true
+#     }
+# }
